544e56fe564e870a635c3c7c3ed3eb3c436d40cb
serena Lee <dearserenalee@gmail.com>
2019-08-15 18:25:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,8 +24,10 @@ function sayHiToGrandma(string){
   if (string === string.toLowerCase()){
     return "I can\'t hear you!"
   }
-  if (string === string.toUpperCase()){
+  else if (string === string.toUpperCase()){
     return "YES INDEED!"
   } 
+  else if (string === "I love you, Grandma."){
+  return "I love you, too."
 }
-
+}
